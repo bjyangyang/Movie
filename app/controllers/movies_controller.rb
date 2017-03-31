d0d219d325fc15b1b030656d4eaea914be6d7e20
chixinfeng <chendongchun520@gmail.com>
2017-03-31 10:02:04 +0800

    join & quit movie action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -42,6 +42,32 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+    def join
+     @movie = Movie.find(params[:id])
+
+      if !current_user.is_member_of?(@movie)
+        current_user.join!(@movie)
+        flash[:notice] = "加入本评论版成功！"
+      else
+        flash[:warning] = "你已经是本评论版成员了！"
+      end
+
+      redirect_to movie_path(@movie)
+    end
+
+    def quit
+      @movie = Movie.find(params[:id])
+
+      if current_user.is_member_of?(@movie)
+        current_user.quit!(@movie)
+        flash[:alert] = "已退出本评论版！"
+      else
+        flash[:warning] = "你不是本评论版成员，怎么退出 XD"
+      end
+
+      redirect_to movie_path(@movie)
+    end
+
   private
   def find_movie_and_check_permission
     @movie = Movie.find(params[:id])
